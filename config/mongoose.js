b8b386e1cc7a37d6a67f17160893718660f2e646
Sujoy Peter <84792655+SujoyPeter@users.noreply.github.com>
2024-04-16 13:22:24 +0530

    Initial commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 // Connect to the MongoDB database using mongoose.connect() method and specify the database URL and options
-mongoose.connect('mongodb+srv://karan:karan@cluster0.gfuxd.mongodb.net/?retryWrites=true&w=majority', 
-{ 
-useNewUrlParser: true,
-useUnifiedTopology: true,
-}
+mongoose.connect('mongodb+srv://sujoypeter:sujoy123@cluster0.ckdun0j.mongodb.net/',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
 // Store the default connection in a variable named db
@@ -13,13 +13,13 @@ const db = mongoose.connection;
 
 // Listen for any errors that may occur in the database connection and log them to the console
 db.on('error', (error) => {
-    console.error('MongoDB connection error:', error);
-  });
+  console.error('MongoDB connection error:', error);
+});
 
 // Once the connection is established, log a message to the console indicating the successful connection
 db.once('open', () => {
-    console.log('MongoDB connected');
-  });
+  console.log('MongoDB connected');
+});
 
 // Export the db object for use in other files
 module.exports = db;
